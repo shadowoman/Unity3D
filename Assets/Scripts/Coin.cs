ad11b9c2e1aca0cdc89dab80bd112819e67b1b3e
Karolina Byrt <karolina.byrt@live.com>
2017-05-08 22:27:51 +0200

    Project Commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    void Start ()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
